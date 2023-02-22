10d335b6cb389328399772e966247d3bd8748e44
Christine Wilks <crissxross@users.noreply.github.com>
2023-02-22 14:28:36 +0000

    Fix syntax error

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -12,7 +12,7 @@ echo "Enter superuser (sudo) password to edit /etc/shells"
 echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
 
-if [ "$SHELL"='/opt/homebrew/bin/zsh' ]: then
+if [ "$SHELL"='/opt/homebrew/bin/zsh' ]; then
   echo '$SHELL is already /opt/homebrew/bin/zsh'
 else
   echo "Enter user password to change login shell"
